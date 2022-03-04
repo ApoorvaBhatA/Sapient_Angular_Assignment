8bab610da8cf9cbbe62153da6a92e0cddd4684f0
Apoorva Bhat <apobhat@WKMZT5B587F6.global.publicisgroupe.net>
2022-03-04 23:11:15 +0530

    implements route2- ecommerce products listing

--- src/app/components/ecommerce-products/ecommerce-products.component.ts
+++ src/app/components/ecommerce-products/ecommerce-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import ecommerceProducts from '@json-data/products.json'
 
 @Component({
   selector: 'app-ecommerce-products',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ecommerce-products.component.scss']
 })
 export class EcommerceProductsComponent implements OnInit {
-
+  products: { title: string; type: string; description: string; filename: string; height: number; width: number; price: number; rating: number; }[] = [];
+  viewFlag: number = 0;
+  
   constructor() { }
 
+  onChangeView(flag: number) {
+    this.viewFlag = flag;
+  }
+
+  sort(event: any) {
+    this.sortProducts(event.target.value)
+  }
+
+  sortProducts(forOption: string) {
+    switch (forOption) {
+      case 'low':
+        this.products = this.products.sort((low, high) => low.price - high.price);
+        break;
+      case 'high':
+        this.products = this.products.sort((low, high) => high.price - low.price);
+        break;
+        default:
+          this.products = this.products.sort((low, high) => high.rating - low.rating);
+    }
+  }
   ngOnInit(): void {
+    this.products = ecommerceProducts;
+    this.sortProducts('popularity')
   }
 
 }
