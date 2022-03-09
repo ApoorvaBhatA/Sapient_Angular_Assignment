c031fc4b745d107348fd23aca44a85c0d5fffcc9
Apoorva Bhat <apobhat@WKMZT5B587F6.global.publicisgroupe.net>
2022-03-09 18:35:12 +0530

    implements infinite scrolling using intersection observer for route6

--- src/app/components/dynamic-div/dynamic-div.component.ts
+++ src/app/components/dynamic-div/dynamic-div.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-div',
@@ -7,15 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicDivComponent implements OnInit {
 
-  items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10","Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10","Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10","Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"];
+  items: string[] = []
+  tempItems = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"];
+  @ViewChild('yourElement') yourElement!: ElementRef;
 
   constructor() { }
 
-  onClickOfButton(i:number) {
+  onClickOfButton(i: number) {
     window.alert(i)
   }
 
   ngOnInit(): void {
+    this.getItems();
+  }
+
+  ngAfterViewInit() {
+    const threshold = 0; 
+    const observer = new IntersectionObserver(
+      () => {
+        this.getItems()
+      },
+      { threshold }
+    );
+    observer.observe(this.yourElement.nativeElement);
+  }
+
+  getItems() {
+    this.tempItems.forEach((element) => {
+      this.items.push(element)
+    })
   }
 
 }
