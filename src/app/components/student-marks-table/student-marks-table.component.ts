200542ba1b05c4afa6938d44ef199f03cd3e0be1
Apoorva Bhat <apobhat@WKMZT5B587F6.global.publicisgroupe.net>
2022-03-09 12:11:16 +0530

    implements track clicks on header

--- src/app/components/student-marks-table/student-marks-table.component.ts
+++ src/app/components/student-marks-table/student-marks-table.component.ts
@@ -7,10 +7,21 @@ import studentsData from '@json-data/students.json'
   styleUrls: ['./student-marks-table.component.scss']
 })
 export class StudentMarksTableComponent implements OnInit {
-  students: { name: string; class: string; section: string; sub1: string; sub2: string; sub3: string; }[]=[];
-  headings : string[] = [];
+  students: { name: string; class: string; section: string; sub1: string; sub2: string; sub3: string; }[] = [];
+  headings: string[] = [];
+  headerClickTrack : {[key: string]: number} = {}
 
   constructor() { }
+
+  onClickOfHeader(header: string) {
+    if (this.headerClickTrack.hasOwnProperty(header)) {
+      this.headerClickTrack[header] = this.headerClickTrack[header] + 1;
+    }
+    else {
+      this.headerClickTrack[header] = 1
+    }
+    console.log(this.headerClickTrack);
+  }
 
   ngOnInit(): void {
     this.students = JSON.parse(JSON.stringify(studentsData));
