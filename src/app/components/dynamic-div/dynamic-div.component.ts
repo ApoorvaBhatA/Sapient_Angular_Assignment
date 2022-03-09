fa13622be9457ef2ffb68b16f1eee4a6f834836d
Apoorva Bhat <apobhat@WKMZT5B587F6.global.publicisgroupe.net>
2022-03-09 16:42:37 +0530

    loops the divs for route6

--- src/app/components/dynamic-div/dynamic-div.component.ts
+++ src/app/components/dynamic-div/dynamic-div.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicDivComponent implements OnInit {
 
+  items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10","Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10","Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10","Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"];
+
   constructor() { }
+
+  onClickOfButton(i:number) {
+    window.alert(i)
+  }
 
   ngOnInit(): void {
   }
