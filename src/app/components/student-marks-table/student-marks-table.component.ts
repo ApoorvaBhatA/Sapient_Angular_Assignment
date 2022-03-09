7ab956c7b0d8dae4eea1dd997d91e558aee0ace7
Apoorva Bhat <apobhat@WKMZT5B587F6.global.publicisgroupe.net>
2022-03-09 12:43:58 +0530

    implements sorting on table - route 5

--- src/app/components/student-marks-table/student-marks-table.component.ts
+++ src/app/components/student-marks-table/student-marks-table.component.ts
@@ -7,7 +7,7 @@ import studentsData from '@json-data/students.json'
   styleUrls: ['./student-marks-table.component.scss']
 })
 export class StudentMarksTableComponent implements OnInit {
-  students: { name: string; class: string; section: string; sub1: string; sub2: string; sub3: string; }[] = [];
+  students: {[key: string]: string | number }[] = [];
   headings: string[] = [];
   headerClickTrack : {[key: string]: number} = {}
 
@@ -15,12 +15,33 @@ export class StudentMarksTableComponent implements OnInit {
 
   onClickOfHeader(header: string) {
     if (this.headerClickTrack.hasOwnProperty(header)) {
-      this.headerClickTrack[header] = this.headerClickTrack[header] + 1;
+      this.headerClickTrack[header] = (this.headerClickTrack[header]>3 ? this.headerClickTrack[header]=0 : this.headerClickTrack[header]) + 1;
     }
     else {
       this.headerClickTrack[header] = 1
     }
-    console.log(this.headerClickTrack);
+    this.sort(header,this.headerClickTrack[header])
+  }
+
+  sort(key:string, sortOrder:number) {
+    if(sortOrder == 1){
+      this.students.sort((a,b)=> (a[key] > b[key]) ? 1 : -1);
+    }
+    else 
+
+    switch(sortOrder) {
+      case 1 :
+        //ascending
+        this.students.sort((a,b)=> (a[key] > b[key]) ? 1 : -1);
+      break;
+      case 2 : 
+        //descending
+        this.students.sort((a,b)=> (a[key] < b[key]) ? 1 : -1);
+      break;
+      default: 
+        //reset
+        this.students = JSON.parse(JSON.stringify(studentsData));
+    }
   }
 
   ngOnInit(): void {
